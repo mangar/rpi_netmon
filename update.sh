bf9048aafcbcc057f3d2c45419f9ab99b1aaa138
Marcio Garcia <marcio.garcia@rga.com>
2018-02-27 14:08:01 -0300

    script de atualizacao com o gist correto

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-USER_SCRIPT=https://gist.githubusercontent.com/mangar/728c09a28b3acf808644f14157b2b214/raw/a1109bd56e625ac6b7a7b5ea13ee6b9f2d3ca014/main.go
-OUTPUT_SCRIPT=/app/user/bin/command.py
+USER_SCRIPT=https://gist.githubusercontent.com/mangar/42616e81e0914ccab6bb15acb7620612/raw/515afee46a6728a8eb9774d6d1e5f73813f211e8/command_dev.py
+OUTPUT_SCRIPT=/home/pi/rpi_netmon-master/user/bin/command_dev.py
 
 wget -q $USER_SCRIPT -O $OUTPUT_SCRIPT
